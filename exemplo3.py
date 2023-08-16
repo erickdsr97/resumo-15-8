ed8ad8afa8308c70edddd876d0894870d05ad6d3
erickdsr97 <83182743+erickdsr97@users.noreply.github.com>
2023-08-15 21:47:47 -0300

    exemplo 3 da aula do dia 15/08

---
+++ exemplo3.py
@@ -0,0 +1,12 @@
+#nome = input()
+
+#print('O nome digitado foi:',nome)
+tomada1 = int(input())
+tomada2 = int(input())
+tomada3 = int(input())
+tomada4 = int(input())
+
+quantidadeTotal = tomada1 + tomada2 + tomada3 + tomada4 - 3
+print(quantidadeTotal)
+
+#resolução sobre a questão do beecrowd
