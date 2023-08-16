21791c719345510a56469d9a34888af8f01c308c
erickdsr97 <83182743+erickdsr97@users.noreply.github.com>
2023-08-15 21:33:03 -0300

    exemplos dia 15-08

---
+++ exemplo.py
@@ -0,0 +1,9 @@
+print('Olá, Mundo!')
+print('oi')
+print('erick')
+
+
+x = 5
+y = 'Olá, mundo!'
+print(x)
+print(y)
---
+++ exemplo2.py
@@ -0,0 +1,30 @@
+# x = 10 
+# y = 20
+x = y = 20
+z = x + y
+print(z)
+
+a = int(3) #exibe os números inteiros
+b = float(3) #exibe os números com decimais
+c = str(3) #converte para string
+
+"""
+também pode ser utilizado para realizar comentários sem ser lido pelo programa
+"""
+print(a)
+#print(type(a)) #utilizado para mostrar o tipo de váriavel que está sendo utilizado
+print(b)
+print(c)
+#print(type(c))
+#todo nome de váriavel começa com letras, underline serve para espaçar a váriavel na descrição.
+
+print(a,b,c) #pode ser utilizado para printar todos na mesma linha
+
+#Camel Case, tem como padrão a primeira letra da váriavel minúscula
+minhaVariavelNova = 2
+
+#Pascal case, toda palavra tem que ter a letra maiúscula
+MinhaVariavelNova = 2
+
+#Snake Case, separando as palavras da váriavel com underline, com as letras todas minúsculas
+minha_variavel_nova = 2
